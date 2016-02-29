f977ef047d7c86d7e98c11df469eb6d475d743bd
Eric Greene <Eric Greene>
2016-02-28 17:20:39 -0700

    hello world demo

--- src/www/js/site.jsx
+++ src/www/js/site.jsx
@@ -1,1 +1,17 @@
 "use strict";
+
+const
+  React = require("react"),
+	ReactDOM = require("react-dom");
+	
+let
+	HelloWorld = React.createClass({
+
+		render: function() {
+			return React.createElement("h1", null, "Hello World!");
+		}
+		
+	});
+	
+ReactDOM.render(React.createElement(HelloWorld),
+	document.querySelector("[class='container']"));
